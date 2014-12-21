03267a1010f0152a16ecb1a5e0d43354ff9e7cc8
Abhijeet Sidhu <abhijeet.sidhu@gmail.com>
2014-12-21 10:33:46 -0500

    Custom widget update

--- widget-cat.php
+++ widget-cat.php
@@ -12,16 +12,29 @@ class Category_Menu extends WP_Widget {
     }
     
     public function widget ($args, $instance) {
-        
+        echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+		echo $args['after_widget'];
     }
     
     public function form($instance) {
+        $title = !empty($instance['title'])? $instance['title']:'New Title';
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>">Title</label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+        </p>
+        <?php
         
     }
     
     public function update($new_instance, $old_instance) {
+        $instance = array();
+        $instance['title'] = (!empty($new_instance['title']))? strip_tags($new_instance['title']): '';
         
-        
+        return $instance;
     }
 }
 
