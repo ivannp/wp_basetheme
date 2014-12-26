e171762689c10319171863487dda6573dd0e58a0
Abhijeet Sidhu <abhijeet.sidhu@gmail.com>
2014-12-26 05:19:46 +0100

    Updates by Ivan

--- content.php
+++ content.php
@@ -47,5 +47,5 @@
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 
-	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', ', ', '</span></footer>' ); ?>
 </article><!-- #post-## -->
